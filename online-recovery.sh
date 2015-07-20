0109ca98af2c0558eb36fb5a6b220a6e03a97a15
Cảnh Nguyễn <canhminhit@gmail.com>
2015-07-20 15:20:41 +0700

    Update online-recovery.sh

--- online-recovery.sh
+++ online-recovery.sh
@@ -8,6 +8,8 @@ postgres_configdir='/home/postgres/data'
 postgres_user_key='/home/postgres/.ssh/id_rsa'
 #Pgpool configuration directory
 pgpool_configdir='/etc/pgpool'
+# Postgres password
+#postgres_password = '123456'
 
 if [ -f '/tmp/postgres_master' ]
 then
@@ -19,11 +21,11 @@ else
 fi
 
 #Get postgres master name
-current_master_name=$(pcp_node_info 10 localhost 9898 postgres postgres $current_master_id | cut -d' ' -f1)
+current_master_name=$(pcp_node_info 10 localhost 9898 postgres 123456 $current_master_id | cut -d' ' -f1)
 #Get postgres slave id
 [ $current_master_id == 0 ] && current_slave_id=1 || current_slave_id=0
 #Get postgres slave name
-current_slave_name=$(pcp_node_info 10 localhost 9898 postgres postgres $current_slave_id | cut -d' ' -f1)
+current_slave_name=$(pcp_node_info 10 localhost 9898 postgres 123456 $current_slave_id | cut -d' ' -f1)
 
 #Test if pgpool is running
 CheckIfPgpoolIsRunning () {
@@ -36,9 +38,9 @@ AttachNodeToPgpool () {
    #pcp_attach_node dont return a good error code when it fails so here if I catch "BackendError" message in stderr I presume
    #that attachment failed.
    #TODO:find a condition to break the folowing loop if attachment fails.
-   while [ "`pcp_attach_node 10 localhost 9898 postgres postgres  $1`" == "BackendError" ]
+   while [ "`pcp_attach_node 10 localhost 9898 postgres 123456  $1`" == "BackendError" ]
     do
-        pcp_attach_node 10 localhost 9898 postgres postgres  $1;
+        pcp_attach_node 10 localhost 9898 postgres 123456  $1;
         #This sleep is recommanded to avoid stressing pgpool in this infinite loop.
         sleep 5;
     done
@@ -49,9 +51,9 @@ ReattachDegeneratedSlave () {
     #Reboot slave node
     echo "[INFO] Slave node '$current_slave_name' is down. Performing postgres server reboot..."
     #Remote postgres reboot via ssh
-    ssh -i $postgres_user_key postgres@$current_slave_name "/etc/init.d/postgresql-9.1 restart"
+    ssh -i $postgres_user_key root@$current_slave_name "/etc/init.d/postgresql-9.1 restart"
     #Test if postgres is running
-    status=$(ssh -i $postgres_user_key postgres@$current_slave_name "if ! killall -0 postgres; then echo 'error'; else echo 'running'; fi;")
+    status=$(ssh -i $postgres_user_key postgres@$current_slave_name "if ! killall -0 postmaster; then echo 'error'; else echo 'running'; fi;")
     if [ $status == "error" ]
     then
         echo "[ERROR] Postgres slave still down !";
@@ -85,7 +87,7 @@ SwitchOldMasterToSlave () {
     new_slave_id=$current_master_id
     #Setup old master config to slave mode
     echo "[INFO] Setting up configuration for the new slave node '$new_slave_name'..."
-    ssh -i $postgres_user_key postgres@$new_slave_name "/etc/init.d/postgresql-9.1 stop"
+    ssh -i $postgres_user_key root@$new_slave_name "/etc/init.d/postgresql-9.1 stop"
     ssh -i $postgres_user_key postgres@$new_slave_name "cp -p $postgres_configdir/postgresql.conf.slave $postgres_configdir/postgresql.conf"
     ssh -i $postgres_user_key postgres@$new_slave_name  "[ -f $postgres_datadir/recovery.done ] && mv $postgres_datadir/recovery.done $postgres_datadir/recovery.conf"
     # Switch slave to new master
@@ -94,8 +96,8 @@ SwitchOldMasterToSlave () {
     ssh -i $postgres_user_key postgres@$new_master_name "[ -f $postgres_datadir/recovery.conf ] && mv $postgres_datadir/recovery.conf $postgres_datadir/recovery.done"
     ssh -i $postgres_user_key postgres@$new_master_name "cp -p $postgres_configdir/postgresql.conf.master $postgres_configdir/postgresql.conf"
     echo "[INFO] Restarting new master..."
-    ssh -i $postgres_user_key postgres@$new_master_name "/etc/init.d/postgresql-9.1 restart"
-    status=$(ssh -i $postgres_user_key postgres@$new_master_name "if ! killall -0 postgres; then echo 'error'; else echo 'running'; fi;")
+    ssh -i $postgres_user_key root@$new_master_name "/etc/init.d/postgresql-9.1 restart"
+    status=$(ssh -i $postgres_user_key postgres@$new_master_name "if ! killall -0 postmaster; then echo 'error'; else echo 'running'; fi;")
     if [ $status == "error" ]
     then
     	echo "[ERROR] New postgres master not running !";
@@ -105,7 +107,7 @@ SwitchOldMasterToSlave () {
     fi
     # Start new slave/master with online recovery
     echo "[INFO] Performing online slave recovery..."
-    ssh -i $postgres_user_key postgres@$new_slave_name "bash /var/lib/postgresql/streaming-replication.sh $new_master_name"
+    ssh -tt -i $postgres_user_key root@$new_slave_name "bash /home/postgres/data/streaming-replication.sh $new_master_name"
     echo "[OK] Online recovery completed."
 
     #Write changes to pgpool.conf file to keep the same current master and slave nodes even after pgpool reboot.
@@ -128,8 +130,8 @@ SwitchOldMasterToSlave () {
 CheckIfPgpoolIsRunning
 
 #Get master/slave state
-current_master_state=$(pcp_node_info 10 localhost 9898 postgres postgres $current_master_id | cut -d' ' -f3)
-current_slave_state=$(pcp_node_info 10 localhost 9898 postgres postgres $current_slave_id | cut -d' ' -f3)
+current_master_state=$(pcp_node_info 10 localhost 9898 postgres 123456 $current_master_id | cut -d' ' -f3)
+current_slave_state=$(pcp_node_info 10 localhost 9898 postgres 123456 $current_slave_id | cut -d' ' -f3)
 
 # state 1 => postgres server is attached but still not receiving connections
 # state 2 => postgres server is attached and managing clients connections
